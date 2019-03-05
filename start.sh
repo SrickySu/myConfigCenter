fb3f64e565f8adb7f939426e8a2aa1b0234678d9
su.qingjia <su.qingjia@yuexiu-finance.com>
2019-03-05 11:45:38 +0800

    新建启动脚本

---
+++ start.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+nohup java -jar /home/mySpringCloud/jar/myEurekaServer-0.0.1-SNAPSHOT.jar > /home/mySpringCloud/log/myEurekaServer.log 2>&1 &
+nohup java -jar /home/mySpringCloud/jar/myWeb-0.0.1-SNAPSHOT.jar > /home/mySpringCloud/log/myWeb.log 2>&1 &
